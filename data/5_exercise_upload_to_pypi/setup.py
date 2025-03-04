5e65ce8ce3b2b6726511e48c7eae73341b13b11f
Aminat Bakare <aminatdamilola50@gmail.com>
2025-03-04 15:18:40 +0000

    Package code

---
+++ data/5_exercise_upload_to_pypi/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_iloyi_grace',
+      version='0.1',
+      description='Gaussian distributions and Binomial Distribution',
+      packages=['distributions'],
+      zip_safe=False)
